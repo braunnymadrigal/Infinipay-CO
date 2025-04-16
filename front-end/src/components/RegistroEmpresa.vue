a302816a87efc01a498bc840e7662f64389547ea
Mafer <maria.herreragutierrez@ucr.ac.cr>
2025-04-16 04:13:02 -0600

    Base para implementar el formulario para registro de una empresa.

---
+++ front-end/src/components/RegistroEmpresa.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Datos de la empresa</h2>
+    <form @submit.prevent="submitForm">
+
+      <div class="button-container">
+        <button @click="$emit('previous')">Volver</button>
+        <button @click="submitForm">Siguiente</button>
+      </div>
+
+      <button type="submit">Terminar registro</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  methods: {
+    submitForm() {
+    }
+  }
+};
+</script>
