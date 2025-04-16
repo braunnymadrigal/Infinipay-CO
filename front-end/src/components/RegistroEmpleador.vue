4de2100bb2a126dd1fab46613b10db2a8166587d
Mafer <maria.herreragutierrez@ucr.ac.cr>
2025-04-16 15:42:04 -0600

    Agregar el logo de la empresa al componente y modificación de colores de botones.

--- front-end/src/components/RegistroEmpleador.vue
+++ front-end/src/components/RegistroEmpleador.vue
@@ -1,26 +1,28 @@
 <template>
   <div>
+    <!-- Logo de la empresa -->
+    <div class="logo_empresa">
+      <img src="../assets/logo.png" alt="Logo de la empresa" class="logo_img"/>
+    </div>
     <!-- Header -->
     <header class="header">
       <div class="navbar">
         <nav class="navbar-links">
           <!-- Contenedor de los botones de la izquierda -->
           <div class="navbar-links-left">
-            <a href="#" class="botones_izquierda" style="color: #FFFFFF;">Personal</a>
-            <a href="#" class="botones_izquierda" style="color: #FFFFFF;">Empresa</a>
+            <a href="#" class="botones_izquierda">Personal</a>
+            <a href="#" class="botones_izquierda">Empresa</a>
           </div>
-
           <!-- Contenedor de los enlaces principales -->
           <div class="navbar-links-center">
             <a href="#"> Inicio</a>
             <a href="#"> Sobre Nosotros</a>
             <a href="#"> ¿Tenés dudas?</a>
           </div>
-
           <!-- Contenedor de los botones de la derecha -->
           <div class="navbar-links-right">
-            <a href="#" class="botones_derecha" style="color: #FFFFFF;">Ingresar</a>
-            <a href="#" class="botones_derecha" style="color: #FFFFFF;">Registrá tu empresa</a>
+            <a href="#" class="botones_derecha">Ingresar</a>
+            <a href="#" class="botones_derecha">Registrá tu empresa</a>
           </div>
         </nav>
       </div>
