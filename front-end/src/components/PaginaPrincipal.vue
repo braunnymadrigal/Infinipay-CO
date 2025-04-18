3bc1b114db28faf59c2ffa31a4faeb246b09db0f
Mafer <maria.herreragutierrez@ucr.ac.cr>
2025-04-17 21:48:21 -0600

    Crear componente pagina principal tomando el header como base de otros componentes

---
+++ front-end/src/components/PaginaPrincipal.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!-- Logo de la empresa -->
+    <div class="logo_empresa">
+      <img src="../assets/images/logo.png" alt="Logo de la empresa" class="logo_img"/>
+    </div>
+    <!-- Header -->
+    <header class="header">
+      <div class="navbar">
+        <nav class="navbar-links">
+          <!-- Contenedor de los botones de la izquierda -->
+          <div class="navbar-links-left">
+            <a href="#" class="botones_izquierda">Personal</a>
+            <a href="#" class="botones_izquierda">Empresa</a>
+          </div>
+          <!-- Contenedor de los botones de la derecha -->
+          <div class="navbar-links-right">
+            <a href="#" class="botones_derecha">Sobre nosotros</a>
+            <a href="#" class="botones_derecha">¿Necesitás ayuda?</a>
+          </div>
+        </nav>
+      </div>
+    </header>
+
+  </div>
+</template>
+
+<style scoped>
+@import '../assets/css/PaginaPrincipal.css';
+</style>
