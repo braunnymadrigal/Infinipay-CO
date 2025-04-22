983ec77ecc7372fa14b969eb2ab96f3d77d722bf
Mafer <maria.herreragutierrez@ucr.ac.cr>
2025-04-22 01:36:31 -0600

    Cambiar en el router los nombres de componentes a inglés

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,20 +1,23 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'primeicons/primeicons.css';
 
 import {createRouter, createWebHistory} from 'vue-router'
-import PantallaPrincipal from './components/PantallaPrincipal.vue'
-import SobreNosotros from './components/SobreNosotros.vue'
-import RegistroEmpleador from './components/RegistroEmpleador.vue'
-import RegistroEmpresa from './components/RegistroEmpresa.vue'  
+import HomePage from './components/HomePage.vue'
+import AboutUs from './components/AboutUs.vue'
+import RegisterEmployer from './components/RegisterEmployer.vue'
+import RegisterCompany from './components/RegisterCompany.vue'  
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    {path: '/', name: "Home", component: PantallaPrincipal},
-    {path: '/sobre_nosotros', name: "SobreNosotros", component: SobreNosotros},
-    {path: '/registrar_empleador', name: "RegistroEmpleador", component: RegistroEmpleador},
-    {path: '/registrar_empresa', name: "RegistroEmpresa", component: RegistroEmpresa},
+    {path: '/', name: "HomePage", component: HomePage},
+    {path: '/AboutUs', name: "AboutUs", component: AboutUs},
+    {path: '/RegisterEmployer', name: "RegisterEmployer"
+      , component: RegisterEmployer},
+    {path: '/RegisterCompany', name: "RegisterCompany"
+      , component: RegisterCompany},
   ]
 });
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount('#app') 
