f4f0649ab8853021f64c16e15d6c1f5cd2bc14d2
braunny.madrigal <braunny.madrigal@ucr.ac.cr>
2025-04-27 16:40:51 -0600

    El footer ha sido agregado al componente.

--- front-end/src/components/LoginUser.vue
+++ front-end/src/components/LoginUser.vue
@@ -22,6 +22,47 @@
 
   <!-- CONTENT -->
   <div> Hola. </div>
+
+  <!-- FOOTER -->
+  <footer class="footer">
+    <div>
+      <p class="footer_text">Contacto</p>
+      <p class="footer_text">Infinipay CO.</p>
+      <div class="contenedor_redes">
+        <a href="#" class="fa fa-facebook"></a>
+        <a href="#" class="fa fa-linkedin"></a>
+        <a href="#" class="fa fa-youtube"></a>
+        <a href="#" class="fa fa-instagram"></a>
+      </div>
+    </div>
+
+    <div>
+      <p class="footer_subtitulo">Empresa y equipo</p>
+      <div class="contenedor_enlaces">
+        <a href="#">Sobre nosotros</a>
+      </div>
+    </div>
+
+    <div >
+      <p class="footer_subtitulo">Recursos</p>
+      <div class="contenedor_enlaces">
+        <a href="#">¿Cómo registro mi empresa?</a>
+        <a href="#">¿Cómo registro empleados a mi empresa?</a>
+        <a href="#">¿Cómo accedo a mi perfil?</a>
+      </div>
+    </div>
+
+    <div>
+      <p class="footer_subtitulo">Contacto</p>
+      <div class="contenedor_enlaces">
+        <!-- Iconos obtenidos de https://www.w3schools.com/css/css_icons.asp -->
+        <a href="#"> <i class="material-icons" style="color:#405D72;">phone</i>
+            +506 2000-0000</a>
+        <a href="#"> <i class="material-icons" style="color:#405D72;">home</i>
+            San José, Montes de Oca, San Pedro</a>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
