f509bf76cde24d2a5c8016993588625674fd8c34
braunny.madrigal <braunny.madrigal@ucr.ac.cr>
2025-04-27 16:35:08 -0600

    El header (de las paginas principales) ha sido agregado.

--- front-end/src/components/LoginUser.vue
+++ front-end/src/components/LoginUser.vue
@@ -1,7 +1,27 @@
 <template>
+  <!-- HEADER -->
   <div>
-
+    <div class="logo_empresa">
+      <img src="../assets/images/logo.png" alt="Logo de la empresa"
+        class="logo_img"/>
+    </div>
+    <header class="header">
+      <div class="navbar">
+        <nav class="navbar-links">
+          <div class="navbar-links-left">
+            <a href="/login_user" class="botones_izquierda">Iniciar sesión</a>
+            <router-link to="/registrar_empleador" class="botones_izquierda">Registrá tu empresa</router-link>
+          </div>
+          <div class="navbar-links-right">
+            <router-link to="/" class="botones_derecha">Página principal</router-link>
+          </div>
+        </nav>
+      </div>
+    </header>
   </div>
+
+  <!-- CONTENT -->
+  <div> Hola. </div>
 </template>
 
 <script>
