ab96b7a1bcdd73bce04bf960f8416b7979c22201
braunny.madrigal <braunny.madrigal@ucr.ac.cr>
2025-04-26 21:42:37 -0600

    Query para crear ambas bases de datos listo.

---
+++ back-end/back-end/SQLQueries/README.md
@@ -0,0 +1,9 @@
+﻿# How to update migrations?
+
+Run the command, only once, after executing the 'SQLQuery001.sql'.
+
+Run the following command on the 'Package Manager Console':
+
+```bash
+Update-Database
+```
---
+++ back-end/back-end/SQLQueries/SQLQuery001.sql
@@ -0,0 +1,14 @@
+﻿use [master];
+GO
+
+-- Creates the database needed to use ASP.NET Identity.
+-- It is structured thanks to automatic migrations.
+create database [AspNetIdentityDB]; 
+GO
+
+use [master];
+GO
+
+-- Creates the database needed to use the Infinipay app.
+create database [InfinipayDB]; 
+GO
