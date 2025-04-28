6954edc65e51eae6a953177fd741681ba29a6554
Fabian <fabian.moraarias@ucr.ac.cr>
2025-04-27 20:23:10 -0600

    Inicialización y estructura básica de las vistas de los beneficios ademas de el enlace con el enrutador

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,20 +1,44 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import {createRouter, createWebHistory} from 'vue-router'
-import PantallaPrincipal from './components/PantallaPrincipal.vue'
-import SobreNosotros from './components/SobreNosotros.vue'
-import RegistroEmpleador from './components/RegistroEmpleador.vue'
-import RegistroEmpresa from './components/RegistroEmpresa.vue'  
+import { createRouter, createWebHistory } from "vue-router";
+import PantallaPrincipal from "./components/PantallaPrincipal.vue";
+import SobreNosotros from "./components/SobreNosotros.vue";
+import RegistroEmpleador from "./components/RegistroEmpleador.vue";
+import RegistroEmpresa from "./components/RegistroEmpresa.vue";
+import RegistroBeneficio from "./components/RegistroBeneficio.vue";
+import ListaBeneficios from "./components/ListaBeneficios.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    {path: '/', name: "Home", component: PantallaPrincipal},
-    {path: '/sobre_nosotros', name: "SobreNosotros", component: SobreNosotros},
-    {path: '/registrar_empleador', name: "RegistroEmpleador", component: RegistroEmpleador},
-    {path: '/registrar_empresa', name: "RegistroEmpresa", component: RegistroEmpresa},
-  ]
+    { path: "/", name: "Home", component: PantallaPrincipal },
+    {
+      path: "/sobre_nosotros",
+      name: "SobreNosotros",
+      component: SobreNosotros,
+    },
+    {
+      path: "/registrar_empleador",
+      name: "RegistroEmpleador",
+      component: RegistroEmpleador,
+    },
+    {
+      path: "/registrar_empresa",
+      name: "RegistroEmpresa",
+      component: RegistroEmpresa,
+    },
+    {
+      path: "/registrar_beneficio",
+      name: "RegistroBeneficio",
+      component: RegistroBeneficio,
+    },
+    {
+      path: "/lista_beneficios",
+      name: "ListaBeneficios",
+      component: ListaBeneficios,
+    },
+  ],
 });
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount("#app");
