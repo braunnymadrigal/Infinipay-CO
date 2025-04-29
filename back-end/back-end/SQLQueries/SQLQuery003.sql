63b26f9a2790a9614259cab37d5567b753de7e0b
24danielcr <24danielcr@gmail.com>
2025-04-29 00:24:45 -0600

    Renombramiento de atributos de tablas Persona y Direccion

---
+++ back-end/back-end/SQLQueries/SQLQuery003.sql
@@ -0,0 +1,16 @@
+﻿EXEC sp_rename 'Persona.Id', 'id', 'COLUMN';
+EXEC sp_rename 'Persona.CorreoElectronico', 'correoElectronico', 'COLUMN';
+EXEC sp_rename 'Persona.TipoIdentificacion', 'tipoIdentificacion', 'COLUMN';
+EXEC sp_rename 'Persona.Identificacion', 'identificacion', 'COLUMN';
+EXEC sp_rename 'Persona.NumeroTelefono', 'numeroTelefono', 'COLUMN';
+EXEC sp_rename 'Persona.IdDatosAuditoria', 'idDatosAuditoria', 'COLUMN';
+EXEC sp_rename 'Unique_TipoIdentificacion_Identificacion', 'UQ_Persona_tipoIdentificacion_identificacion';
+EXEC sp_rename 'Unique_NumeroTelefono', 'UQ_Persona_numeroTelefono';
+
+EXEC sp_rename 'Direccion.IdPersona', 'idPersona', 'COLUMN';
+EXEC sp_rename 'Direccion.Provincia', 'provincia', 'COLUMN';
+EXEC sp_rename 'Direccion.Canton', 'canton', 'COLUMN';
+EXEC sp_rename 'Direccion.Distrito', 'distrito', 'COLUMN';
+EXEC sp_rename 'Direccion.OtrasSeñas', 'otrasSeñas', 'COLUMN';
+
+
