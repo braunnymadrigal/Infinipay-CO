ac30a5944685c582385f435afde23c57e8ca7f7f
24danielcr <24danielcr@gmail.com>
2025-04-29 00:07:49 -0600

    Query para creación de table Persona y sus atributos
    Se creo la tabla Persona y Dirección, renombramiento a tabla.

---
+++ back-end/back-end/SQLQueries/SQLQuery002.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE Personas (
+	Id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+	CorreoElectronico VARCHAR(100) UNIQUE NOT NULL,
+	TipoIdentificacion VARCHAR(50) UNIQUE NOT NULL,
+	Identificacion INT NOT NULL,
+	IdDatosAuditoria UNIQUEIDENTIFIER,
+	CONSTRAINT Unique_TipoIdentificacion_Identificacion UNIQUE (TipoIdentificacion, Identificacion)
+);
+
+ALTER TABLE Personas
+Add NumeroTelefono VARCHAR(8) NOT NULL,
+CONSTRAINT Unique_NumeroTelefono UNIQUE (NumeroTelefono);
+
+CREATE TABLE Direccion (
+	IdPersona UNIQUEIDENTIFIER,
+	Provincia VARCHAR(50) NOT NULL,
+	Canton VARCHAR(50) NOT NULL,
+	Distrito VARCHAR(50) NOT NULL,
+	OtrasSeñas VARCHAR(50) NOT NULL
+	CONSTRAINT FK_Direccion_Persona FOREIGN KEY (IdPersona) REFERENCES Personas(Id)
+)
+
+EXEC sp_rename 'Personas', 'Persona';
