e3abdf09fd3727ea93f16c9977818824a193fd61
24danielcr <24danielcr@gmail.com>
2025-04-29 00:45:27 -0600

    Creación tablas de Datos de Auditoría, Persona Física y Persona Jurídica

---
+++ back-end/back-end/SQLQueries/SQLQuery004.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE DatosAuditoria (
+    id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+    fechaCreacion DATETIME2 NOT NULL,
+    usuarioCreador VARCHAR(100) NOT NULL,
+    UltimaFechaModificacion DATETIME2,
+    UltimoUsuarioModificador VARCHAR(100)
+);
---
+++ back-end/back-end/SQLQueries/SQLQuery005.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE PersonaFisica(
+	Id UNIQUEIDENTIFIER PRIMARY KEY,
+	primerNombre VARCHAR(50) NOT NULL,
+	segundoNombre VARCHAR(50) NOT NULL,
+	primerApellido VARCHAR(50) NOT NULL,
+	segundoApellido VARCHAR(50) NOT NULL,
+	CONSTRAINT FK_PersonaFisica_Persona FOREIGN KEY (Id) REFERENCES Persona(Id)
+);
+
+CREATE TABLE PersonaJuridica(
+	Id UNIQUEIDENTIFIER PRIMARY KEY,
+	descripcion VARCHAR(250) NOT NULL,
+	tipoPago VARCHAR(30) NOT NULL,
+	beneficiosPorEmpleado TINYINT NOT NULL,
+	razonSocial VARCHAR(254) NOT NULL,
+	CONSTRAINT FK_PersonaJuridica_Persona FOREIGN KEY (Id) REFERENCES Persona(Id)
+);
