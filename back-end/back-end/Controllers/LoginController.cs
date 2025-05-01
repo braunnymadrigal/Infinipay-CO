465cf5c3433b044d7eb59aca6c3f885855cb9f17
braunny.madrigal <braunny.madrigal@ucr.ac.cr>
2025-04-30 23:02:39 -0600

    creado el LoginController, de momento solo se crea su constructor

---
+++ back-end/back-end/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace back_end.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        private SqlConnection _connection;
+        private string? _pathConnection;
+
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+            var builder = WebApplication.CreateBuilder();
+            _pathConnection = builder.Configuration.GetConnectionString("InfinipayDBContext");
+            _connection = new SqlConnection(_pathConnection);
+        }
+    }
+}
