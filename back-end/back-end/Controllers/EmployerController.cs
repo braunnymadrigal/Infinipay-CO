0d5beed165567c3351416b30cbc1841b45165208
Mafer <maria.herreragutierrez@ucr.ac.cr>
2025-05-02 02:47:42 -0600

    Controlador de registro de empleador

---
+++ back-end/back-end/Controllers/EmployerController.cs
@@ -0,0 +1,39 @@
+﻿using back_end.Handlers;
+using back_end.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace back_end.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class EmployerController : ControllerBase
+  {
+    private readonly EmployerRepository _employerRepository;
+    
+    public EmployerController()
+    {
+      _employerRepository = new EmployerRepository();
+    }
+    [HttpPost]
+    public async Task<ActionResult<bool>>createNewEmployer(EmployerModel
+      employer)
+    {
+      try
+      {
+        if (employer == null)
+        {
+          return BadRequest();
+        }
+        EmployerRepository employerRepository = new EmployerRepository();
+        var result = employerRepository.createNewEmployer(employer);
+        return new JsonResult(result);
+      }
+      catch (Exception)
+      {
+        return StatusCode(StatusCodes.Status500InternalServerError
+          , "Error creando empleador");
+      }
+    }
+  }
+}
