07c3cbce3733e4d4acaed9f3ae22f45c2cf949eb
Mafer <maria.herreragutierrez@ucr.ac.cr>
2025-05-02 02:49:47 -0600

    Modelo inicial de registrar empleado con algunos datos (hacen falta)

---
+++ back-end/back-end/Models/EmployerModel.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace back_end.Models
+{
+
+  public class EmployerModel
+  {
+    [Required]
+    public int idNumber { get; set; }
+    
+    [Required]
+    public string phoneNumber { get; set; }
+
+    [Required]
+    public string email { get; set; }
+
+    [Required]
+    public string firstName { get; set; }
+
+    [Required]
+    public string secondName { get; set; }
+
+    [Required]
+    public string firstLastName { get; set; }
+
+    [Required]
+    public string secondLastName { get; set; }
+
+    [Required]
+    public string province { get; set; }
+
+    [Required]
+    public string canton { get; set; }
+
+    [Required]
+    public string district { get; set; }
+
+    public string otherSigns { get; set; }
+
+    [Required]
+    public string idType { get; set; }
+  }
+}
