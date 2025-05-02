8dbe1399212111278ffbd008f5972174782231f2
24danielcr <24danielcr@gmail.com>
2025-05-02 11:42:37 -0600

    Correcion creación de BD

--- back-end/back-end/SQLQueries/SQLQuery001.sql
+++ back-end/back-end/SQLQueries/SQLQuery001.sql
@@ -1,14 +1,3 @@
-﻿use [master];
-GO
-
--- Creates the database needed to use ASP.NET Identity.
--- It is structured thanks to automatic migrations.
-create database [AspNetIdentityDB]; 
-GO
-
-use [master];
-GO
-
--- Creates the database needed to use the Infinipay app.
+﻿-- Creates the database needed to use the Infinipay app.
 create database [InfinipayDB]; 
 GO
