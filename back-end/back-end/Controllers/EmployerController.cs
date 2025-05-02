31e617837f33e56bd39552b5ec1ab56daf87dced
Mafer <maria.herreragutierrez@ucr.ac.cr>
2025-05-02 16:12:43 -0600

    Agregar validacion de errores para que el usuario pueda ver el error especifico

--- back-end/back-end/Controllers/EmployerController.cs
+++ back-end/back-end/Controllers/EmployerController.cs
@@ -5,6 +5,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace back_end.Controllers
 {
+  public class ErrorResponse
+  {
+    public string Message { get; set; }
+    public string Details { get; set; }
+  }
+
   [Route("api/[controller]")]
   [ApiController]
   public class EmployerController : ControllerBase
@@ -29,10 +35,36 @@ namespace back_end.Controllers
         var result = employerRepository.createNewEmployer(employer);
         return new JsonResult(result);
       }
-      catch (Exception)
+      catch (Exception ex)
       {
+        var errorResponse = new { message = "", details = ex.Message };
+
+        if (ex.Message.Contains("CEDULA_DUPLICADA"))
+        {
+          return Conflict(new {
+            message = "Error: ya existe un empleador registrado con" +
+            "esa cédula." });
+        }
+        else if (ex.Message.Contains("TELEFONO_DUPLICADO"))
+        {
+          return Conflict(new {
+            message = "Error: ya existe un empleador registrado con ese" +
+            "número de teléfono." });
+        }
+        else if (ex.Message.Contains("EMAIL_DUPLICADO"))
+        {
+          return Conflict(new {
+            message = "Error: ya existe un empleador registrado con ese" +
+            "correo electrónico." });
+        }
+        else if (ex.Message.Contains("USERNAME_DUPLICADO"))
+        {
+          return Conflict(new {
+            message = "Error: ya existe un empleador registrado con ese" +
+            "nombre de usuario." });
+        }
         return StatusCode(StatusCodes.Status500InternalServerError
-          , "Error creando empleador");
+          , new { message = "Error creando empleador", details = ex.Message });
       }
     }
   }
