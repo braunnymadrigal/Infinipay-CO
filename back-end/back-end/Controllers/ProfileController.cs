8db527806dee760961539548b20298272514ef0d
braunny.madrigal <braunny.madrigal@ucr.ac.cr>
2025-05-03 21:31:56 -0600

    listo el obtener los datos que contiene el jwt

--- back-end/back-end/Controllers/ProfileController.cs
+++ back-end/back-end/Controllers/ProfileController.cs
@@ -2,6 +2,8 @@
 using back_end.Models;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace back_end.Controllers
 {
@@ -14,6 +16,29 @@ namespace back_end.Controllers
         public ProfileController()
         {
             _profileRepository = new ProfileRepository();
+        }
+
+        [Authorize]
+        [HttpGet]
+        public ProfileModel Get()
+        {
+            string tablaPersonaId = "";
+            ProfileModel profileModel = new ProfileModel();
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                var nameIdentifier = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value;
+                var sid = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Sid)?.Value;
+                var role = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value;
+                if (nameIdentifier != null && sid != null && role != null)
+                {
+                    profileModel.NombreUsuario = nameIdentifier;
+                    tablaPersonaId = sid;
+                    profileModel.Rol = role;
+                }
+            }
+            return profileModel;
         }
     }
 }
