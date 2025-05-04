4c77fd78dd0ed23d61cf6e302966bb25d27c8344
Mafer <maria.herreragutierrez@ucr.ac.cr>
2025-05-03 23:47:40 -0600

    Nuevo query para editar constraint y agregar un nuevo rol a la base de datos

---
+++ back-end/back-end/SQLQueries/SQLQuery013.sql
@@ -0,0 +1,8 @@
+﻿ALTER TABLE Empleado
+DROP CONSTRAINT CHK_Empleado_rol;
+
+ALTER TABLE Empleado
+ADD CONSTRAINT CHK_Empleado_rol CHECK (rol IN ('supervisor', 'administrador', 'empleado'));
+
+ALTER TABLE Empleado
+ALTER COLUMN rol VARCHAR(15) NOT NULL;
