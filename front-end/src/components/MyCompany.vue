f3ba18a78a1a3f47dc69a922429825cf4b6ecdb6
braunny.madrigal <braunny.madrigal@ucr.ac.cr>
2025-05-03 18:25:54 -0600

    setup basico para empezar a desarrollar el componente

---
+++ front-end/src/components/MyCompany.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+
+  </div>
+
+  <!-- FOOTER -->
+  <MainFooter/>
+</template>
+
+<script>
+  import MainFooter from "./MainFooter.vue";
+  export default {
+    components: {
+      MainFooter
+    },
+  };
+</script>
+
+<style>
+  @import '../assets/css/HeaderFooter.css';
+</style>
