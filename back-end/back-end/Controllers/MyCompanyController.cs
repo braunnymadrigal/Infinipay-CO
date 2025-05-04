cd22c1af3d935f3874689cdcdb516286f6e180f9
braunny.madrigal <braunny.madrigal@ucr.ac.cr>
2025-05-04 13:55:26 -0600

    setup basico para empezar el desarrollo de esta parte del backend

---
+++ back-end/back-end/Controllers/MyCompanyController.cs
@@ -0,0 +1,13 @@
+﻿using back_end.Repositories;
+using back_end.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace back_end.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyCompanyController : ControllerBase
+    {
+    }
+}
