63948f488ef40713640acfa7df7421e07c9f3568
braunny.madrigal <braunny.madrigal@ucr.ac.cr>
2025-05-04 14:37:26 -0600

    listo el metodo para obtener como string la celda en primer fila primer columna de una tabla
    ademas, listo el caso en que un super admin intenta usar esta funcionalidad

--- back-end/back-end/Repositories/MyCompanyRepository.cs
+++ back-end/back-end/Repositories/MyCompanyRepository.cs
@@ -19,7 +19,30 @@ namespace back_end.Repositories
         public MyCompanyModel Get(string ownerId)
         {
             MyCompanyModel myCompanyModel = new MyCompanyModel();
+            string query = $"SELECT [idPersonaJuridica] FROM [Empleador] WHERE [idPersonaFisica] = '{ownerId}';";
+            string companyId = "";
+            companyId = GetFirstColumnFirstRow(query);
+            if (companyId != "")
+            {
+                myCompanyModel.Benefits = "manin";
+            }
             return myCompanyModel;
+        }
+
+        private string GetFirstColumnFirstRow(string query)
+        {
+            string returnValue = "";
+            DataTable table = CreateTable(query);
+            if (table.Rows.Count > 0)
+            {
+                DataRow rowResult = table.Rows[0];
+                var cellResult = Convert.ToString(rowResult[0]);
+                if (cellResult != "" && cellResult != null)
+                {
+                    returnValue = cellResult;
+                }
+            }
+            return returnValue;
         }
 
         private DataTable CreateTable(string query)
