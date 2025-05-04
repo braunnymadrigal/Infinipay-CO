0d1a097c46fbd3098934eb5b4a323cf5d224a065
Mafer <maria.herreragutierrez@ucr.ac.cr>
2025-05-04 09:49:05 -0600

    Eliminar redundacia

--- back-end/back-end/SQLQueries/SQLQuery013.sql
+++
@@ -1,8 +0,0 @@
-﻿ALTER TABLE Empleado
-DROP CONSTRAINT CHK_Empleado_rol;
-
-ALTER TABLE Empleado
-ADD CONSTRAINT CHK_Empleado_rol CHECK (rol IN ('supervisor', 'administrador', 'empleado'));
-
-ALTER TABLE Empleado
-ALTER COLUMN rol VARCHAR(15) NOT NULL;
