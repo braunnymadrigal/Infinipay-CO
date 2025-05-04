2d06f36d6ee8c3897798d76ce50353d1084bea84
24danielcr <24danielcr@gmail.com>
2025-05-03 18:55:59 -0600

    Base backend, beneficios en la BD en Swagger se muestran.

---
+++ back-end/back-end/back-end/Controllers/AssignedBenefitListRepository.cs
@@ -0,0 +1,29 @@
+﻿using back_end.Repositories;
+using back_end.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace back_end.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+
+  public class AssignedBenefitListController : ControllerBase
+  {
+    private readonly AssignedBenefitListRepository
+      _assignedBenefitListRepository;
+
+    public AssignedBenefitListController()
+    {
+      _assignedBenefitListRepository = new AssignedBenefitListRepository();
+    }
+
+    [HttpGet]
+    public List<AssignedBenefitListModel> Get()
+    {
+      var benefits = _assignedBenefitListRepository.GetBenefits();
+      return benefits;
+    }
+  }
+}
---
+++ back-end/back-end/back-end/Repositories/AssignedBenefitListRepository.cs
@@ -0,0 +1,62 @@
+﻿using back_end.Models;
+using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace back_end.Repositories
+{
+  public class AssignedBenefitListRepository
+  {
+    private SqlConnection _connection;
+    private string _connectionRoute;
+
+    public AssignedBenefitListRepository()
+    {
+      var builder = WebApplication.CreateBuilder();
+      _connectionRoute =
+        builder.Configuration.GetConnectionString("InfinipayCO");
+      _connection = new SqlConnection(_connectionRoute);
+    }
+
+    private DataTable GetQueryTable(string query)
+    {
+      SqlCommand queryCommand = new SqlCommand(query, _connection);
+      SqlDataAdapter tableAdapter = new SqlDataAdapter(queryCommand);
+      DataTable queryTable = new DataTable();
+      _connection.Open();
+      tableAdapter.Fill(queryTable);
+      _connection.Close();
+      return queryTable;
+    }
+
+    public List<AssignedBenefitListModel> GetBenefits()
+    {
+      List<AssignedBenefitListModel> benefitsList
+        = new List<AssignedBenefitListModel>();
+      string query = "SELECT * FROM [dbo].[Beneficio]";
+      DataTable tableResult = GetQueryTable(query);
+
+      foreach (DataRow column in tableResult.Rows)
+      {
+        benefitsList.Add(
+          new AssignedBenefitListModel
+          {
+            benefitId = (Guid)column["id"],
+            benefitName = Convert.ToString(column["nombre"]),
+            benefitMinTime = Convert.ToDecimal(column["tiempoMinimo"]),
+            benefitDescription = Convert.ToString(column["descripcion"]),
+            benefitElegibleEmployees =
+              Convert.ToString(column["empleadoElegible"]),
+
+            benefitAudit = (Guid)column["idAuditoria"],
+            companyId = (Guid)column["idPersonaJuridica"]
+          });
+      }
+      return benefitsList;
+    }
+
+  }
+}
