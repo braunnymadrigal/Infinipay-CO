c77a1557aff6076d9280d601eff51055f2b9b4d0
braunny.madrigal <braunny.madrigal@ucr.ac.cr>
2025-05-03 20:32:33 -0600

    agregado setup basico para el controller y el repository de Profile

---
+++ back-end/back-end/Controllers/ProfileController.cs
@@ -0,0 +1,19 @@
+﻿using back_end.Repositories;
+using back_end.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace back_end.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        private readonly ProfileRepository _profileRepository;
+
+        public ProfileController()
+        {
+            _profileRepository = new ProfileRepository();
+        }
+    }
+}
---
+++ back-end/back-end/Repositories/ProfileRepository.cs
@@ -0,0 +1,19 @@
+﻿using back_end.Models;
+
+using Microsoft.Data.SqlClient;
+
+namespace back_end.Repositories
+{
+    public class ProfileRepository
+    {
+        private readonly SqlConnection _connection;
+        private readonly string? _pathConnection;
+
+        public ProfileRepository()
+        {
+            var builder = WebApplication.CreateBuilder();
+            _pathConnection = builder.Configuration.GetConnectionString("InfinipayDBContext");
+            _connection = new SqlConnection(_pathConnection);
+        }
+    }
+}
