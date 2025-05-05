39586e8ce96d062da9993dc242a7188c59322204
Mafer <maria.herreragutierrez@ucr.ac.cr>
2025-05-05 01:41:28 -0600

    Controlador para lista de empleados

---
+++ back-end/back-end/Controllers/EmployeeListController.cs
@@ -0,0 +1,24 @@
+﻿using back_end.Repositories;
+using back_end.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace back_end.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class EmployeeListController : ControllerBase
+  {
+    private readonly EmployeeListRepository _employeeListRepository;
+    public EmployeeListController()
+    {
+      _employeeListRepository = new EmployeeListRepository();
+    }
+    [HttpGet]
+    public List<EmployeeListModel> Get()
+    {
+      var employees = _employeeListRepository.obtainEmployeeInfo();
+      return employees;
+    }
+  }
+}
