b3f80e6ad3dd8d97e9c0b8d719d40bd6b50c8182
24danielcr <24danielcr@gmail.com>
2025-05-04 21:30:34 -0600

    Diferenciar entre empleado y empleador para Beneficios.

--- front-end/src/components/HeaderCompany.vue
+++ front-end/src/components/HeaderCompany.vue
@@ -1,41 +1,62 @@
-<template>
+﻿<template>
   <div class="container my-5">
     <div class="text-center mb-4">
-      <img
-        src="../assets/images/logo.png"
-        alt="Company logo"
-        class="img-fluid"
-        style="max-width: 350px"
-      />
+      <img src="../assets/images/logo.png"
+           alt="Company logo"
+           class="img-fluid"
+           style="max-width: 350px" />
     </div>
-      <header class="mb-5 custom-header">
-        <nav class="navbar navbar-expand-lg rounded custom-navbar">
-          <div class="container-fluid">
-            <div class="d-flex">
-              <router-link
-                to="/MyProfile"
-                class="mx-2"
-                style="color: #405d72"
-                >Perfil</router-link
-              >
-              <router-link
-                to="/MyCompany"
-                class="mx-2"
-                style="color: #405d72"
-                >Empresa</router-link
-              >
-              <router-link to="/BenefitList" class="mx-2" style="color: #405d72"
-                >Beneficios</router-link
-              >
-              <a href="#" class="mx-2" style="color: #405d72">Empleados</a>
-            </div>
+    <header class="mb-5 custom-header">
+      <nav class="navbar navbar-expand-lg rounded custom-navbar">
+        <div class="container-fluid">
+          <div class="d-flex">
+            <router-link to="/MyProfile"
+                         class="mx-2"
+                         style="color: #405d72">Perfil</router-link>
+            <router-link to="/MyCompany"
+                         class="mx-2"
+                         style="color: #405d72">Empresa</router-link>
+            <a @click="goToBenefits" class="mx-2" style="color: #405d72; cursor: pointer;">
+              Beneficios
+            </a>
+            <a href="#" class="mx-2" style="color: #405d72">Empleados</a>
           </div>
-        </nav>
-      </header>
-    </div>
+        </div>
+      </nav>
+    </header>
+  </div>
 
 </template>
-<script setup></script>
+<script>
+  import axios from "axios";
+  export default {
+    methods: {
+      async goToBenefits() {
+        try {
+          const response = await axios.get("https://localhost:7275/api/Login/GetLoggedUser", {
+            headers: {
+              Authorization: `Bearer ${this.$cookies.get('jwt')}`
+            }
+          });
+
+          const role = response.data.Role;
+
+          if (role === 'empleado') {
+            this.$router.push('/AssignedBenefitList');
+          } else {
+            this.$router.push('/BenefitList');
+          }
+        } catch (error) {
+          console.error("Error fetching user role:", error);
+          this.$router.push('/MyCompany');
+        }
+      }
+    }
+
+  }
+
+</script>
+
 <style>
   @import '../assets/css/HeaderFooter.css';
 </style>
