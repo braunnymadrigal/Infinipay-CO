f11c3ba5610d59a779239dbdc35a94fe33078322
Mafer <maria.herreragutierrez@ucr.ac.cr>
2025-05-24 12:26:42 -0600

    Controlador para el calculo de impuesto de renta

---
+++ back-end/back-end/API/RentTaxCalculationController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using back_end.Infraestructure;
+using back_end.Application;
+using System.Security.Claims;
+
+namespace back_end.API
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class RentTaxCalculationController : ControllerBase
+  {
+    private readonly RentTax rentTaxCalculation;
+
+    public RentTaxCalculationController()
+    {
+      var dataBaseConnection = new DataBaseConnectionRepository();
+      var rentTaxRepository =
+        new RentTaxRepository(dataBaseConnection);
+
+      var monthlyTaxStrategy = new MonthlyEmployeeRentTax();
+
+      rentTaxCalculation = new RentTax(rentTaxRepository
+        , monthlyTaxStrategy);
+    }
+
+    [HttpGet]
+    public ActionResult GetRentTax()
+    {
+      try
+      {
+        // string employeeID = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        // TEST
+        string employeeID = "09B49DDE-EBD5-4D00-8871-059FDD92352A";
+        if (string.IsNullOrEmpty(employeeID))
+          return Unauthorized(new { mensaje = "Usuario no autenticado." });
+
+        var taxAmount = rentTaxCalculation.CalculateRentTax(employeeID);
+
+        return Ok(new
+        {
+          calculatedTax = taxAmount,
+          employeeID = employeeID
+        });
+      }
+      catch (Exception ex)
+      {
+        return BadRequest(new { mensaje = ex.Message });
+      }
+    }
+  } 
+}
