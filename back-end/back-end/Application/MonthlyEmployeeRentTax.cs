ea8f243f1d5fd9290f9b169efb3cb3eb314aa421
Mafer <maria.herreragutierrez@ucr.ac.cr>
2025-05-24 12:27:40 -0600

    Clases con los calculos para el impuesto de renta

---
+++ back-end/back-end/Application/MonthlyEmployeeRentTax.cs
@@ -0,0 +1,41 @@
+﻿namespace back_end.Application
+{
+  public class MonthlyEmployeeRentTax : IRentTax
+  {
+    public decimal CalculateRentTax(decimal salary)
+    {
+      decimal tax = 0;
+
+      switch (salary)
+      {
+        case <= 922_000:
+          tax = 0;
+          break;
+
+        case <= 1_352_000:
+          tax = (salary - 922_000m) * 0.10m;
+          break;
+
+        case <= 2_373_000:
+          tax = (1_352_000m - 922_000m) * 0.10m +
+                (salary - 1_352_000m) * 0.15m;
+          break;
+
+        case <= 4_745_000:
+          tax = (1_352_000m - 922_000m) * 0.10m +
+                (2_373_000m - 1_352_000m) * 0.15m +
+                (salary - 2_373_000m) * 0.20m;
+          break;
+
+        default:
+          tax = (1_352_000m - 922_000m) * 0.10m +
+                (2_373_000m - 1_352_000m) * 0.15m +
+                (4_745_000m - 2_373_000m) * 0.20m +
+                (salary - 4_745_000m) * 0.25m;
+          break;
+      }
+
+      return Math.Round(tax, 2);
+    }
+  }
+}
---
+++ back-end/back-end/Application/RentTax.cs
@@ -0,0 +1,42 @@
+﻿using back_end.Infraestructure;
+namespace back_end.Application
+{
+  public class RentTax
+  {
+    private readonly RentTaxRepository _repository;
+    private readonly IRentTax _taxStrategy;
+    public RentTax(RentTaxRepository repository,
+      IRentTax taxStrategy)
+    {
+      _repository = repository;
+      _taxStrategy = taxStrategy;
+    }
+    public decimal CalculateRentTax(string employeeId)
+    {
+      var (contractType, grossSalary) =
+        _repository.GetEmployeeSalaryInfo(employeeId);
+
+      decimal estimatedGrossSalary;
+
+      switch (contractType)
+      {
+        case "mensual":
+          estimatedGrossSalary = grossSalary;
+          break;
+
+        case "quincenal":
+          estimatedGrossSalary = grossSalary;
+          break;
+
+        case "semanal":
+          estimatedGrossSalary = grossSalary;
+          // TODO: lógica de cálculo de salario semanal
+          break;
+
+        default:
+          throw new Exception("Tipo de contrato no reconocido.");
+      }
+      return _taxStrategy.CalculateRentTax(estimatedGrossSalary);
+    }
+  }
+}
