d4b8f9d32d517fee38e9b8f62494ef6b20c1d67d
Mafer <maria.herreragutierrez@ucr.ac.cr>
2025-05-24 12:26:12 -0600

    Método modularizado para la conexión a la base de datos

---
+++ back-end/back-end/Infraestructure/DataBaseConnectionRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace back_end.Infraestructure
+{
+  public class DataBaseConnectionRepository
+  {
+    private readonly string _connectionString;
+
+    public DataBaseConnectionRepository()
+    {
+      var builder = WebApplication.CreateBuilder();
+      _connectionString =
+        builder.Configuration.GetConnectionString("InfinipayDBContext");
+    }
+
+    public DataTable ExecuteQuery(string query)
+    {
+      using var connection = new SqlConnection(_connectionString);
+      using var command = new SqlCommand(query, connection);
+      var adapter = new SqlDataAdapter(command);
+      var table = new DataTable();
+      connection.Open();
+      adapter.Fill(table);
+      return table;
+    }
+  }
+}
