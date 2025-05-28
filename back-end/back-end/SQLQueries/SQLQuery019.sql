15d29455d64fde0c9058c923703cbf3dfaafd0f2
24danielcr <24danielcr@gmail.com>
2025-05-27 22:59:08 -0600

    Se elimina fecha y monto, se agrega fecha inicio y fin, salario bruto y neto a DetallePago.

---
+++ back-end/back-end/SQLQueries/SQLQuery019.sql
@@ -0,0 +1,34 @@
+﻿USE [InfinipayDB];
+GO
+
+ALTER TABLE DetallePago
+DROP CONSTRAINT UQ_DetallePago_fecha_idPlanilla_idEmpleado;
+GO
+
+ALTER TABLE DetallePago
+DROP COLUMN fecha;
+GO
+
+ALTER TABLE DetallePago
+ADD CONSTRAINT UQ_DetallePago_idPlanilla_idEmpleado UNIQUE(idPlanilla, idEmpleado);
+GO
+
+ALTER TABLE DetallePago
+DROP COLUMN monto;
+GO
+
+ALTER TABLE DetallePago
+ADD fechaInicio DATE NOT NULL;
+GO
+
+ALTER TABLE DetallePago
+ADD fechaFin DATE NOT NULL;
+GO
+
+ALTER TABLE DetallePago
+ADD salarioBruto DECIMAL(11,2) NOT NULL DEFAULT 0.0;
+GO
+
+ALTER TABLE DetallePago
+ADD salarioNeto DECIMAL(11,2) NOT NULL DEFAULT 0.0;
+GO
