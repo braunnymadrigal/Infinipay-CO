2dbd87c392aaa5df0c7360d9101a835d901c9067
24danielcr <24danielcr@gmail.com>
2025-05-27 22:58:03 -0600

    Se agrega estado, fecha inicio y fin a tabla Planilla.

---
+++ back-end/back-end/SQLQueries/SQLQuery017.sql
@@ -0,0 +1,19 @@
+﻿USE [InfinipayDB];
+GO
+
+ALTER TABLE Planilla
+ADD fechaInicio DATE NOT NULL;
+GO
+
+ALTER TABLE Planilla
+ADD fechaFin DATE NOT NULL;
+GO
+
+ALTER TABLE Planilla
+ADD estado VARCHAR(11) NOT NULL;
+GO
+
+ALTER TABLE Planilla
+ADD CONSTRAINT CHK_Planilla_estado 
+CHECK (estado IN ('en progreso', 'completado', 'error'));
+GO
