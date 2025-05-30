c1c84c439e2292a5cc3e9fd1da200a0955ffe4c0
braunnymadrigal <braunnyjmb@gmail.com>
2025-05-29 23:37:36 -0600

    creado el controlador GrossSalary
    aun no tiene la logica final

---
+++ back-end/back-end/API/GrossSalaryController.cs
@@ -0,0 +1,38 @@
+﻿using back_end.Application;
+using back_end.Domain;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace back_end.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GrossSalaryController : GeneralController
+    {
+        //private readonly IGrossSalary grossSalary;
+
+        public GrossSalaryController()
+        {
+            //grossSalary = new GrossSalary();
+        }
+
+        [Authorize(Roles = "empleador")]
+        [HttpPost]
+        public IActionResult ComputeGrossSalary(DateOnly startDate, DateOnly endDate)
+        {
+            IActionResult iActionResult = BadRequest("Unknown error.");
+            try
+            {
+                var id = GetUser().PersonId;
+                //iActionResult = Ok(token);
+                iActionResult = Ok(id);
+            }
+            catch (Exception e)
+            {
+                iActionResult = NotFound(e.Message);
+            }
+            return iActionResult;
+        }
+    }
+}
