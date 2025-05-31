076f70d9277d33f384ecad557475534c1389bb2e
Mafer <Mafer@LAPTOP-0P3BI91S>
2025-05-31 13:46:05 -0600

    Endpoint para probar si funciona el calculo de impuesto de renta

---
+++ back-end/back-end/API/RentTaxController.cs
@@ -0,0 +1,36 @@
+﻿using back_end.Application;
+using back_end.Domain;
+using back_end.Infraestructure;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace back_end.API
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class RentTaxController : GeneralController
+  {
+    private readonly IRentTax rentTax;
+    public RentTaxController()
+    {
+      rentTax = new RentTax();
+    }
+
+    [Authorize(Roles = "empleador")]
+    [HttpPost]
+    public IActionResult ComputeRentTaxes(List<GrossSalaryModel> grossSalaries)
+    {
+      IActionResult iActionResult = BadRequest("Unknown error.");
+      try
+      {
+        var rentTaxes = rentTax.calculateRentTaxes(grossSalaries);
+        iActionResult = Ok(rentTaxes);
+      }
+      catch (Exception e)
+      {
+        iActionResult = NotFound(e.Message);
+      }
+      return iActionResult;
+    }
+  }
+}
