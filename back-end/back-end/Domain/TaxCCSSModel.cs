b4ce619a04c1d55d248b33c943d4fa25a520386c
braunnymadrigal <braunnyjmb@gmail.com>
2025-05-31 09:39:35 -0600

    agregado el modelado a utilizar

---
+++ back-end/back-end/Domain/TaxCCSSModel.cs
@@ -0,0 +1,8 @@
+﻿namespace back_end.Domain
+{
+    public class TaxCCSSModel
+    {
+        public required double EmployeeAmount { get; set; }
+        public required double EmployerAmount { get; set; }
+    }
+}
