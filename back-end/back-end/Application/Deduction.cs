ec49836241dd934a7272aa4fb88a22b35ac19429
braunnymadrigal <braunnyjmb@gmail.com>
2025-06-01 23:38:19 -0600

    lista la modularazion de las apis, ahora si bien require el server conocer la api de antemano, esta mejor implementado
    ahora las apis usan los valores variables obtenido desde la base de datos. Falta refinar el metodo para extraer la respuesta

--- back-end/back-end/Application/Deduction.cs
+++ back-end/back-end/Application/Deduction.cs
@@ -9,6 +9,11 @@ namespace back_end.Application
         private const string DEDUCTION_BY_API = "api";
         private const string DEDUCTION_BY_FIXED_AMOUNT = "montoFijo";
         private const string DEDUCTION_BY_PERCENTAGE = "porcentaje";
+        private const string JSON_ENCODING = "application/json";
+        private const string MALE_IN_ENGLISH = "male";
+        private const string MALE_IN_SPANISH = "masculino";
+        private const string FEMALE_IN_ENGLISH = "female";
+        private const string FEMALE_IN_SPANISH = "femenino";
 
         private const string VORLAGENERSTELLER_API_URL = 
             "https://mediseguro-vorlagenersteller-d4hmbvf7frg7aqan.southcentralus-01.azurewebsites.net/api/MediSeguroMonto";
@@ -67,21 +72,23 @@ namespace back_end.Application
             switch (deduction.apiUrl)
             {
                 case VORLAGENERSTELLER_API_URL:
-                    myJsonDoc = await ApiVorlagenersteller();
+                    myJsonDoc = await apiVorlagenersteller(deduction, employee);
                     break;
                 case FRIENDS_API_URL:
-                    myJsonDoc = await ApiFriends();
+                    myJsonDoc = await apiFriends(deduction, employee);
                     break;
                 case GEEMS_API_URL:
-                    myJsonDoc = await ApiGeems();
+                    myJsonDoc = await apiGeems(deduction, employee);
                     break;
                 default:
                     throw new Exception("Unknown API can not be used.");
             }
-            var extractedValue = ExtractDoubleValue(myJsonDoc);
+            var extractedValue = extractDoubleValue(myJsonDoc);
             deduction.resultAmount = extractedValue;
             return deduction;
         }
+
+
 
 
         private double convertStringToDouble(string value)
@@ -96,123 +103,81 @@ namespace back_end.Application
             return convertedValue;
         }
 
-        //[Authorize(Roles = "empleador")]
-        //[HttpPost]
-        //public async Task<IActionResult> ComputeMoreDeductions(int type)
-        //{
-        //    IActionResult iActionResult = BadRequest("Unknown error.");
-        //    try
-        //    {
-        //        JsonDocument myJsonDoc;
-        //        switch (type)
-        //        {
-        //            case 1:
-        //                myJsonDoc = await ApiFriends();
-        //                break;
-        //            case 2:
-        //                myJsonDoc = await ApiVorlagenersteller();
-        //                break;
-        //            case 3:
-        //                myJsonDoc = await ApiGeems();
-        //                break;
-        //            default:
-        //                throw new Exception("Bad type");
-        //        }
-        //        double extractedValue = ExtractDoubleValue(myJsonDoc);
-
-        //        iActionResult = Ok(extractedValue);
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        iActionResult = NotFound(e.Message);
-        //    }
-        //    return iActionResult;
-        //}
-
-
-        private async Task<JsonDocument> ApiFriends()
+        private async Task<JsonDocument> apiFriends(PayrollDeductionModel deduction
+            , PayrollEmployeeModel employee)
         {
             using var client = new HttpClient();
-
-            // base URL
-            var baseUrl = "https://poliza-friends-grg0h9g5crf2hwh8.southcentralus-01.azurewebsites.net/api/LifeInsurance";
-
-            // query parameters
-            var requestUrl = $"{baseUrl}?date%20of%20birth=2004-10-12&sex=male";
-
-            // headers
-            client.DefaultRequestHeaders.Add("FRIENDS-API-TOKEN", "1D0B194488C091852597B9AF7D1AA8F23D55C9784815489CF0A488B6F2C6D5C4C569AD51231FACB9920E5A763FE388247E03131D1601AC234E86BC0D266EB6A7");
-
-            // GET
-            var response = await client.GetAsync(requestUrl);
-
-            var responseContent = await response.Content.ReadAsStringAsync();
-
-            var doc = JsonDocument.Parse(responseContent);
-            return doc;
+            var birthDate = Convert.ToString(employee.birthDate.ToString("yyyy-MM-dd"));
+            var gender = convertGenderToEnglish(employee.gender);
+            var url = $"{FRIENDS_API_URL}?{deduction.param1Key}={birthDate}&{deduction.param2Key}={gender}";
+            client.DefaultRequestHeaders.Add(deduction.header1Key, deduction.header1Value);
+            var response = await client.GetAsync(url);
+            return await readApiResponse(response);
         }
 
-        private async Task<JsonDocument> ApiVorlagenersteller()
+        private async Task<JsonDocument> apiVorlagenersteller(PayrollDeductionModel deduction
+            , PayrollEmployeeModel employee)
         {
             using var client = new HttpClient();
-
-            var url = "https://mediseguro-vorlagenersteller-d4hmbvf7frg7aqan.southcentralus-01.azurewebsites.net/api/MediSeguroMonto";
-
-            client.DefaultRequestHeaders.Add("token", "TOKEN123");
-
-            // Create the JSON body
+            client.DefaultRequestHeaders.Add(deduction.header1Key, deduction.header1Value);
             var requestBody = new
             {
-                fechaNacimiento = "2000-10-31",
-                genero = "masculino",
-                cantidadDependientes = 0
+                fechaNacimiento = employee.birthDate,
+                genero = employee.gender,
+                cantidadDependientes = deduction.dependantNumber
             };
-
-            // Serialize to JSON
-            var json = JsonSerializer.Serialize(requestBody);
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
-
-            // Send the POST request
-            var response = await client.PostAsync(url, content);
-
-            // Read the response
-            var responseContent = await response.Content.ReadAsStringAsync();
-
-            var doc = JsonDocument.Parse(responseContent);
-            return doc;
+            var content = serializeToJson(requestBody);
+            var response = await client.PostAsync(VORLAGENERSTELLER_API_URL, content);
+            return await readApiResponse(response);
         }
 
-        private async Task<JsonDocument> ApiGeems()
+        private async Task<JsonDocument> apiGeems(PayrollDeductionModel deduction
+            , PayrollEmployeeModel employee)
         {
             using var client = new HttpClient();
-
-            var url = "https://asociacion-geems-c3dfavfsapguhxbp.southcentralus-01.azurewebsites.net/api/public/calculator/calculate";
-
-            client.DefaultRequestHeaders.Add("API-KEY", "Tralalerotralala");
-
-            // Create the JSON body
+            client.DefaultRequestHeaders.Add(deduction.header1Key, deduction.header1Value);
             var requestBody = new
             {
-                associationName = "GEEMS",
-                employeeSalary = 500000.75
+                associationName = employee.companyAssociation,
+                employeeSalary = employee.computedGrossSalary
             };
-
-            // Serialize to JSON
-            var json = JsonSerializer.Serialize(requestBody);
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
-
-            // Send the POST request
-            var response = await client.PostAsync(url, content);
-
-            // Read the response
-            var responseContent = await response.Content.ReadAsStringAsync();
-
-            var doc = JsonDocument.Parse(responseContent);
-            return doc;
+            var content = serializeToJson(requestBody);
+            var response = await client.PostAsync(GEEMS_API_URL, content);
+            return await readApiResponse(response);
         }
 
+        private StringContent serializeToJson(Object csharpObject)
+        {
+            var json = JsonSerializer.Serialize(csharpObject);
+            var content = new StringContent(json, Encoding.UTF8, JSON_ENCODING);
+            return content;
+        }
 
-        static double ExtractDoubleValue(JsonDocument doc)
+        private async Task<JsonDocument> readApiResponse(HttpResponseMessage response)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var jsonDoc = JsonDocument.Parse(content);
+            return jsonDoc;
+        }
+
+        private string convertGenderToEnglish(string gender)
+        {
+            var translatedGender = "";
+            switch (gender)
+            {
+                case MALE_IN_SPANISH:
+                    translatedGender = MALE_IN_ENGLISH;
+                    break;
+                case FEMALE_IN_SPANISH:
+                    translatedGender = FEMALE_IN_ENGLISH;
+                    break;
+                default:
+                    throw new Exception("Not supported gender");
+            }
+            return translatedGender;
+        }
+
+        private double extractDoubleValue(JsonDocument doc)
         {
             var root = doc.RootElement;
 
