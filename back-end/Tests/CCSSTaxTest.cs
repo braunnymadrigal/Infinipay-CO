f8fca65b3210cc5da6459f0a69a68641c5147603
braunnymadrigal <braunnyjmb@gmail.com>
2025-06-03 20:38:08 -0600

    creada la clase para testear el impuesto de la CCSS

---
+++ back-end/Tests/CCSSTaxTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using back_end.Application;
+using back_end.Domain;
+
+namespace Tests
+{
+    class CCSSTaxTest
+    {
+        ITaxCCSS taxCCSS;
+        List<PayrollEmployeeModel> employees;
+        DateOnly endDate;
+
+        [SetUp]
+        public void Setup()
+        {
+            taxCCSS = new TaxCCSS();
+            employees = new List<PayrollEmployeeModel> {
+                new PayrollEmployeeModel
+                {
+                    id = "",
+                    gender = "",
+                    birthDate = DateOnly.MinValue,
+                    rentTax = 0.0,
+                    rawGrossSalary = 0.0,
+                    computedGrossSalary = 0.0,
+                    ccssEmployeeDeduction = 0.0,
+                    ccssEmployerDeduction = 0.0,
+                    hiringDate = DateOnly.MinValue,
+                    hiringType = "",
+                    hoursDate = DateOnly.MinValue,
+                    hoursNumber = 0,
+                    companyAssociation = "",
+                    deductions = new List<PayrollDeductionModel>(),
+                    previousComputedGrossSalaries = new List<PayrollPreviousComputedGrossSalary>()
+                }
+            };
+        }
+    }
+}
