54091c907f7176e3ad945ea130e61699618c082d
mafer <maria.herreragutierrez@ucr.ac.cr>
2025-06-19 14:53:40 -0600

    Interfaz para el repositorio del reporte de un empleado

---
+++ back-end/back-end/Infraestructure/IEmployeeReportRepository.cs
@@ -0,0 +1,9 @@
+﻿using back_end.Domain;
+
+namespace back_end.Infraestructure
+{
+  public interface IEmployeeReportRepository
+  {
+    public EmployeeFullReport GetEmployeePayrollReport(string employeeId);
+  }
+}
