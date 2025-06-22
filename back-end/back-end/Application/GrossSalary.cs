5bc64c57119689b3fb6ecb53bdf98f5e1989333a
braunnymadrigal <braunnyjmb@gmail.com>
2025-06-22 14:42:38 -0600

    Agregadas validaciones en GrossSalary para los salarios computados.

--- back-end/back-end/Application/GrossSalary.cs
+++ back-end/back-end/Application/GrossSalary.cs
@@ -9,6 +9,10 @@ namespace back_end.Application
         private const string BIWEEKLY_PAYMENT_TYPE = "quincenal";
         private const string MONTHLY_PAYMENT_TYPE = "mensual";
 
+        private const double P_VALUE_ON_DECIMAL_SQL_TYPE = 11.0;
+        private const double S_VALUE_ON_DECIMAL_SQL_TYPE = 2.0;
+        private const double EXPONENTATION_BASE_VALUE_ON_DECIMAL_SQL_TYPE = 10.0;
+
         private IContextGrossSalaryComputation _contextGrossSalaryComputation;
 
         public GrossSalary(IContextGrossSalaryComputation contextGrossSalaryComputation)
@@ -22,6 +26,7 @@ namespace back_end.Application
             setGrossSalaryComputationStrategy(payrollEmployer.paymentType);
             payrollEmployees = _contextGrossSalaryComputation.computeGrossSalary(payrollEmployees, 
                 payrollEmployer.startDate, payrollEmployer.endDate);
+            validateComputedGrossSalaries(payrollEmployees);
             return payrollEmployees;
         }
 
@@ -37,6 +42,42 @@ namespace back_end.Application
                     break;
                 default:
                     throw new Exception("GrossSalary: Improper strategy have been specified.");
+            }
+        }
+
+        private void validateComputedGrossSalaries(List<PayrollEmployeeModel> payrollEmployees)
+        {
+            foreach (var payrollEmployee in payrollEmployees)
+            {
+                validateComputedGrossSalaryGreaterThanZero(payrollEmployee.computedGrossSalary);
+                validateComputedGrossSalarySize(payrollEmployee.computedGrossSalary);
+            }
+        }
+
+        private void validateComputedGrossSalarySize(double computedGrossSalary)
+        {
+            var maxSize =
+                (Math.Pow(
+                    EXPONENTATION_BASE_VALUE_ON_DECIMAL_SQL_TYPE,
+                    (P_VALUE_ON_DECIMAL_SQL_TYPE - S_VALUE_ON_DECIMAL_SQL_TYPE)
+                    )
+                ) -
+                (Math.Pow(EXPONENTATION_BASE_VALUE_ON_DECIMAL_SQL_TYPE
+                , -S_VALUE_ON_DECIMAL_SQL_TYPE));
+            if (computedGrossSalary > maxSize)
+            {
+                throw new Exception("The computed gross salary can not exceed" +
+                  "the database limitations");
+            }
+        }
+
+        private void validateComputedGrossSalaryGreaterThanZero(double
+          computedGrossSalary)
+        {
+            if (computedGrossSalary < 0)
+            {
+                throw new Exception("The computed gross salary can not be" +
+                  "less than zero.");
             }
         }
     }
