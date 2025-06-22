6b5a250a808e38100dca484e8b35abf027f96b94
braunnymadrigal <braunnyjmb@gmail.com>
2025-06-22 11:53:43 -0600

    listo el modelo para albergar todos los impuestos obligatorios

---
+++ back-end/back-end/Domain/PayrollTaxesModel.cs
@@ -0,0 +1,20 @@
+﻿namespace back_end.Domain
+{
+    public class PayrollTaxesModel
+    {
+        public double employeeCcssSem { get; set; }
+        public double employeeCcssIvm { get; set; }
+        public double employeeLptBpop { get; set; }
+        public double employeeRent { get; set; }
+        public double employerCcssSem { get; set; }
+        public double employerCcssIvm { get; set; }
+        public double employerOthersBpop { get; set; }
+        public double employerOthersFamily { get; set; }
+        public double employerOthersImas { get; set; }
+        public double employerOthersIna { get; set; }
+        public double employerLptBpop { get; set; }
+        public double employerLptFcl { get; set; }
+        public double employerLptOpc { get; set; }
+        public double employerLptIns { get; set; }
+    }
+}
