473c9246f92d68114a80d885d2b3c94fbb7f8736
braunnymadrigal <braunnyjmb@gmail.com>
2025-06-22 16:12:38 -0600

    agregada mas precision al calculo de Monthly y Biweekly grossSalary

--- back-end/back-end/Application/BiweeklyGrossSalaryComputation.cs
+++ back-end/back-end/Application/BiweeklyGrossSalaryComputation.cs
@@ -15,8 +15,8 @@ namespace back_end.Application
                 if (payrollEmployee.hiringDate > startDate)
                 {
                     var numberOfWorkedDays = (endDate.Day - payrollEmployee.hiringDate.Day) + 1;
-                    var newGrossSalary = (payrollEmployee.rawGrossSalary / MAXIMUM_DAYS_OF_WORK) * numberOfWorkedDays;
-                    payrollEmployee.computedGrossSalary = newGrossSalary;
+                    payrollEmployee.computedGrossSalary = (double)((Decimal)(payrollEmployee.computedGrossSalary 
+                        / MAXIMUM_DAYS_OF_WORK) * numberOfWorkedDays);
                 }
             }
             return payrollEmployees;
