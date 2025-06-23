68d7baf018bb96871adf7736e4143c77359ed201
braunnymadrigal <braunnyjmb@gmail.com>
2025-06-22 19:29:46 -0600

    Lista la funcionalidad de obtener el tipo de pago de la empresa.
    Es un simple endpoint GET, que solo pueden usar los empleadores y lo que devuelve es semanal, quincenal o mensual segun el tipo de pago de la empresa que posee.

---
+++ back-end/back-end/Application/IPayrollEmployer.cs
@@ -0,0 +1,10 @@
+﻿using back_end.Domain;
+
+namespace back_end.Application
+{
+    public interface IPayrollEmployer
+    {
+        PayrollEmployerModel getPayrollEmployer(string id, DateOnly startDate, DateOnly endDate);
+        string getPaymentType(string id);
+    }
+}
---
+++ back-end/back-end/Application/PayrollEmployer.cs
@@ -0,0 +1,40 @@
+﻿using back_end.Domain;
+using back_end.Infraestructure;
+
+namespace back_end.Application
+{
+    public class PayrollEmployer : IPayrollEmployer
+    {
+        private readonly IPayrollEmployerRepository _payrollEmployerRepository;
+
+        public PayrollEmployer(IPayrollEmployerRepository payrollEmployerRepository)
+        {
+            _payrollEmployerRepository = payrollEmployerRepository;
+        }
+
+        public PayrollEmployerModel getPayrollEmployer(string id, DateOnly startDate, DateOnly endDate)
+        {
+            return new PayrollEmployerModel();
+        }
+
+        public string getPaymentType(string id)
+        {
+            var payrollEmployer = new PayrollEmployerModel
+            {
+                id = id,
+            };
+            payrollEmployer.id = id;
+            payrollEmployer = _payrollEmployerRepository.getPayrollEmployer(payrollEmployer);
+            checkStringNotEmpty(payrollEmployer.paymentType);
+            return payrollEmployer.paymentType;
+        }
+
+        private void checkStringNotEmpty(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new Exception("PayrollEmployer: caught invalid string.");
+            }
+        }
+    }
+}
