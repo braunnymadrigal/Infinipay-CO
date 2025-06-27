4da362f8383a87a833cd81da7160edccfc02f89b
braunnymadrigal <braunnyjmb@gmail.com>
2025-06-27 12:29:11 -0600

    Lista casi toda logica de negocio para borrar empresa.
    Falta lo de enviar correo, pero ocupo que dicha API este lista.

---
+++ back-end/back-end/Application/CompanyDeletion.cs
@@ -0,0 +1,32 @@
+﻿using back_end.Infraestructure;
+
+namespace back_end.Application
+{
+    public class CompanyDeletion : ICompanyDeletion
+    {
+        private readonly ICompanyDeletionRepository _companyDeletionRepository;
+
+        public CompanyDeletion(ICompanyDeletionRepository companyDeletionRepository)
+        {
+            _companyDeletionRepository = companyDeletionRepository;
+        }
+
+        public List<string> deleteCompany(string companyName)
+        {
+            var employerEmail = _companyDeletionRepository.getEmployerEmail(companyName);
+            var employeesEmail = _companyDeletionRepository.getEmployeesEmail(companyName);
+            _companyDeletionRepository.deleteCompany(employerEmail);
+            employeesEmail.Add(employerEmail);
+            sendEmails(employeesEmail);
+            return new List<string>();
+        }
+
+        private void sendEmails(List<string> emails)
+        {
+            for (var i = 0; i < emails.Count; ++i)
+            {
+                //
+            }
+        }
+    }
+}
