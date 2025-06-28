010531ec574305ed8fa3710b3d7fe0a44b3d7941
Fabian <fabian.moraarias@ucr.ac.cr>
2025-06-28 13:06:33 -0600

    Delete fisico de beneficios
    Arreglo para que los get de beneficios no muestren los borraodos
    
    Se implementa el envio de correos a los empleados y se actualiza el selesct de planilla para que maneje correctamente los borrados
    
    Fix a el sendemail de delete beneficios
    
    Mensajes de confirmacion en el front

--- back-end/back-end/Application/ICompanyBenefitCommand.cs
+++ back-end/back-end/Application/ICompanyBenefitCommand.cs
@@ -8,5 +8,6 @@ namespace back_end.Application
       , string loggedUserNickname);
     public void UpdateBenefit(Guid id, CompanyBenefitDTO benefit
       , string loggedUserNickname);
+    public Task DeleteBenefit(Guid id, string loggedUserNickname);
   }
 }
--- front-end/src/plugins/api.js
+++ front-end/src/plugins/api.js
@@ -161,8 +161,7 @@ export default {
       },
 
       sendEmail(email) {
-        return axios.post(`${apiBaseURL}/Email`
-          , email, authHeader());
+        return axios.post(`${apiBaseURL}/Email`, email, authHeader());
       },
 
       getCompanyType() {
@@ -175,6 +174,13 @@ export default {
 
       showEmployeeReport() {
         return axios.get(`${apiBaseURL}/EmployeeReport`, authHeader());
+      },
+
+      deleteCompanyBenefit(benefitId) {
+        return axios.delete(
+          `${apiBaseURL}/CompanyBenefit/${benefitId}`,
+          authHeader()
+        );
       },
     };
   },
