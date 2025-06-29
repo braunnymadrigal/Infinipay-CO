86b7faf986f19e2c7efdbb4d0ac1c48674d10a72
mafer <maria.herreragutierrez@ucr.ac.cr>
2025-06-29 17:46:22 -0600

    Interfaz del orquestador de planilla

---
+++ back-end/back-end/Application/IPayrollOrchestrator.cs
@@ -0,0 +1,11 @@
+﻿using back_end.Domain;
+using back_end.Models;
+
+namespace back_end.Application
+{
+  public interface IPayrollOrchestrator
+  {
+    Task<List<EmployeePayrollResult>> ComputePayrollAsync(string employerId
+      , DateOnly start, DateOnly end);
+  }
+}
