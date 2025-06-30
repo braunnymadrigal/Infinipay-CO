660e0b406231d16cedf2c6d962da0cd1e36d0a55
mafer <maria.herreragutierrez@ucr.ac.cr>
2025-06-29 18:59:07 -0600

    Modelo de datos para el reporte empleado

---
+++ back-end/back-end/Domain/EmployeeReportModel.cs
@@ -0,0 +1,27 @@
+﻿namespace back_end.Domain
+{
+  public class EmployeeFullReport
+  {
+    public string fullName { get; set; }
+    public string contractType { get; set; }
+    public string companyName { get; set; }
+    public List<PaymentReport> payments { get; set; } = new();
+  }
+
+  public class PaymentReport
+  {
+    public decimal computedGrossSalary { get; set; }
+    public decimal netSalary { get; set; }
+    public DateTime startDate { get; set; }
+    public DateTime endDate { get; set; }
+    public List<DeductionDetail> deductions { get; set; } = new();
+  }
+
+  public class DeductionDetail
+  {
+    public string type { get; set; }
+    public decimal amount { get; set; }
+    public string deductionName { get; set; }
+  }
+
+}
